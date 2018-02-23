689550c04bdfd60b1a8e415c417cb6cf8507a3bf
us3r <us3r@mail>
2018-02-23 12:37:35 +0100

    markdown-mode config

--- include/mio-conf.el
+++ include/mio-conf.el
@@ -61,3 +61,11 @@
 
 (when (fboundp 'winner-mode)
   (winner-mode 1))
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
